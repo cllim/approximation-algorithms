ae2d4ffc236719a5c0293ce5a4b31673ed1c1e8f
cllim <chiunlin@gmail.com>
2014-10-16 11:16:25 -0600

    Fix minor errors with main

--- set-cover/main.cpp
+++ set-cover/main.cpp
@@ -11,22 +11,27 @@ using std::vector;
 
 int main(int argc, char* argv[]) {
 	SetCover* problem = SetCover::problemSetup("set-cover-input");
+	
 	vector<double> result;
-	if (!strcmp(argv[1], "LP_rounding"))	
-		result = problem->approx_LP_rounding();
-	else if (!strcmp(argv[1], "dual_rounding"))
-		result = problem->approx_dual_rounding();
-	else if (!strcmp(argv[1], "primal_dual"))
-		result = problem->approx_primal_dual();
-	else if (!strcmp(argv[1], "greedy"))
-		result = problem->approx_greedy();
-	else if (!strcmp(argv[1], "exact"))
-		result = problem->exact_MIP();
+	if (argc > 1){
+		if (!strcmp(argv[1], "LP_rounding"))
+			result = problem->approx_LP_rounding();
+		else if (!strcmp(argv[1], "dual_rounding"))
+			result = problem->approx_dual_rounding();
+		else if (!strcmp(argv[1], "primal_dual"))
+			result = problem->approx_primal_dual();
+		else if (!strcmp(argv[1], "greedy"))
+			result = problem->approx_greedy();
+		else if (!strcmp(argv[1], "exact"))
+			result = problem->exact_MIP();
+		else // default
+			result = problem->exact_MIP();
+	}
 	else // default
-		result = problem->approx_LP_rounding();
-	std::cout << "Total weight: " << result[6] << '\n';
+		result = problem->exact_MIP();
+	std::cout << "Total weight: " << result[problem->subset.size()] << '\n';
 	std::cout << "Set chosen: ";
-	for (int i = 0; i < 6; i++){
+	for (int i = 0; i < problem->subset.size(); i++){
 		std::cout << (int)result[i] << " ";
 	}
 	std::cout << "\n";
